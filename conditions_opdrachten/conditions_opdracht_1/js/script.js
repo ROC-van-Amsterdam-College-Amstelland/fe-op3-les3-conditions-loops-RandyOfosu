3bcdd455dccfcd94d2e402c88717912324e0a6ec
Randy Ofosu <randy_ofosu@hotmail.com>
2019-12-02 15:33:18 +0100

    Opdracht push

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function check() {
+  var saldo = document.getElementById("saldo").value;
+
+  if (saldo < 25) {
+    document.getElementById("tekst").innerHTML = "niet genoeg saldo.";
+  } else {
+    document.getElementById("tekst").innerHTML = "Voldoende saldo!";
+  }
+}
