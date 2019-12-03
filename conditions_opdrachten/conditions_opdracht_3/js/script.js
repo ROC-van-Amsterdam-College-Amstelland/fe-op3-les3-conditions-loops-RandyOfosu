f119c727713399dd71361c692097f3f6e06bb5fd
Randy Ofosu <randy_ofosu@hotmail.com>
2019-12-03 12:55:20 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function check() {
+  var saldo = document.getElementById("saldo").value;
+
+  if (saldo == 500) {
+    document.getElementById("tekst").innerHTML = "Salaris is gestort!";
+  } else if (saldo > 500) {
+    document.getElementById("tekst").innerHTML = "Bonus gestort!!";
+  }
+}
