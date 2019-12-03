4440da53785a5cba7d8695426817df44e408d3f9
Randy Ofosu <randy_ofosu@hotmail.com>
2019-12-03 14:46:16 +0100

    Opdracht 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+function bereken() {
+  const getal1 = parseInt(document.getElementById("getal1").value);
+  const getal2 = parseInt(document.getElementById("getal2").value);
+  let operator = document.getElementById("operator").value;
+
+  if (operator == "+") {
+    document.getElementById("antwoord").innerHTML = getal1 + getal2;
+  }
+  if (operator == "-") {
+    document.getElementById("antwoord").innerHTML = getal1 - getal2;
+  }
+  if (operator == "*") {
+    document.getElementById("antwoord").innerHTML = getal1 * getal2;
+  }
+  if (operator == "/") {
+    document.getElementById("antwoord").innerHTML = getal1 / getal2;
+  }
+}
