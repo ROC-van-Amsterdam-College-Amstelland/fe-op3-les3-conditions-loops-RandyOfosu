e1d6dad8e03e565ef615dd84a2328a88ada37dbb
Randy Ofosu <randy_ofosu@hotmail.com>
2019-12-03 15:55:07 +0100

    Loops 2

--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+function showLoop() {
+  let a;
+  let text = "";
+
+  for (a = 20; a >= 0; a--) {
+    console.log(a);
+    text += "Nummer " + a + "<br>";
+  }
+  document.getElementById("loop").innerHTML = text;
+}
